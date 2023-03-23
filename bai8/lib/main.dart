515339f9bde605854e4e137e14d0fc6841692b94
TranDangHoangGiang <97326266+TranDangHoangGiang@users.noreply.github.com>
2023-03-23 13:53:04 +0700

    Update main.dart

--- bai8/lib/main.dart
+++ bai8/lib/main.dart
@@ -72,38 +72,40 @@ class _TodoListScreenState extends State<TodoListScreen> {
       builder: (context) {
         return AlertDialog(
           title: Text('Thêm công việc mới'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: titleController,
-                decoration: InputDecoration(
-                  labelText: 'Tên công việc',
-                ),
-              ),
-              TextField(
-                controller: contentController,
-                minLines: 1,
-                maxLines: 5,
-                keyboardType: TextInputType.multiline,
-                decoration: InputDecoration(
-                  labelText: 'Nội dung công việc',
-                ),
-              ),
-              SizedBox(height: 16),
-              InkWell(
-                onTap: () => _selectDate(context, deadlineController),
-                child: AbsorbPointer(
-                  child: TextField(
-                    controller: deadlineController,
-                    decoration: InputDecoration(
-                      labelText: 'Thời hạn chót (VD: 2023-03-31)',
-                    ),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: titleController,
+                  decoration: InputDecoration(
+                    labelText: 'Tên công việc',
                   ),
-                  // onTap: () => _selectDate(context),
                 ),
-              ),
-            ],
+                TextField(
+                  controller: contentController,
+                  minLines: 1,
+                  maxLines: 5,
+                  keyboardType: TextInputType.multiline,
+                  decoration: InputDecoration(
+                    labelText: 'Nội dung công việc',
+                  ),
+                ),
+                SizedBox(height: 16),
+                InkWell(
+                  onTap: () => _selectDate(context, deadlineController),
+                  child: AbsorbPointer(
+                    child: TextField(
+                      controller: deadlineController,
+                      decoration: InputDecoration(
+                        labelText: 'Thời hạn chót (VD: 2023-03-31)',
+                      ),
+                    ),
+                    // onTap: () => _selectDate(context),
+                  ),
+                ),
+              ],
+            ),
           ),
           actions: [
             TextButton(
@@ -144,47 +146,50 @@ class _TodoListScreenState extends State<TodoListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Công việc của tôi',
-          style: TextStyle(color: Colors.white),
+        appBar: AppBar(
+          title: Text(
+            'Công việc của tôi',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Color.fromARGB(255, 171, 143, 246),
         ),
-        backgroundColor: Colors.blueAccent,
-      ),
-      body: ListView.builder(
-        itemCount: _tasks.length,
-        itemBuilder: (context, index) {
-          final task = _tasks[index];
-          final deadlineFormatted =
-              DateFormat('dd/MM/yyyy').format(task.deadline);
+        body: ListView.builder(
+          itemCount: _tasks.length,
+          itemBuilder: (context, index) {
+            final task = _tasks[index];
+            final deadlineFormatted =
+                DateFormat('dd/MM/yyyy').format(task.deadline);
 
-          return ExpansionTile(
-            title:
-                Text(task.title, style: TextStyle(fontWeight: FontWeight.bold)),
-            // contentPadding: (task.content),
-            subtitle: Text('Hạn chót: $deadlineFormatted'),
-            trailing:
-                task.isOverdue() ? Icon(Icons.error, color: Colors.red) : null,
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    task.content,
-                    style: TextStyle(fontSize: 16.0),
+            return ExpansionTile(
+              title: Text(task.title,
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              // contentPadding: (task.content),
+              subtitle: Text('Hạn chót: $deadlineFormatted'),
+              trailing: task.isOverdue()
+                  ? Icon(Icons.error, color: Colors.red)
+                  : null,
+              children: [
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      task.content,
+                      style: TextStyle(fontSize: 16.0),
+                    ),
                   ),
                 ),
-              ),
-            ],
-          );
-        },
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addTask,
-        child: Icon(Icons.add),
-      ),
-    );
+              ],
+            );
+          },
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        floatingActionButton: FloatingActionButton(
+          onPressed: _addTask,
+          // backgroundColor: Colors.deepPurpleAccent,
+          child: Icon(Icons.add),
+        ),
+        backgroundColor: Color.fromARGB(255, 220, 227, 234));
   }
 }
